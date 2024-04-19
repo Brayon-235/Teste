7bdc7dae70ebabf00597b0407c0afb583cb90c47
Brayon <brayon081@gmail.com>
2024-04-19 10:21:48 -0400

    Teste

---
+++ lab 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+   static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um numero: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite um segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        double soma = num + num2;
+        Console.WriteLine("A soma desses números é: " + soma);
+        
+
+        Console.ReadKey();
+    }
+}
