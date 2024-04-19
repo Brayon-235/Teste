3daa401fe2d32552beb83285dde96c65af2e7630
Brayon <brayon081@gmail.com>
2024-04-19 10:36:45 -0400

    segundo commit

--- lab 2/Program.cs
+++ lab 2/Program.cs
@@ -5,12 +5,16 @@ class Program
     {
         Console.WriteLine("Digite um numero: ");
         double num = Convert.ToDouble(Console.ReadLine());
-
+	
+	/*
         Console.WriteLine("Digite um segundo número: ");
         int num2 = Convert.ToInt32(Console.ReadLine());
+	*/
 
         double soma = num + num2;
         Console.WriteLine("A soma desses números é: " + soma);
+
+	
         
 
         Console.ReadKey();
